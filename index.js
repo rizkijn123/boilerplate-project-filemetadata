8218b8ff7584171d48b1aecb02fb75ebedbaf921
rizki Jaelani <rizkijaelani11@gmail.com>
2024-10-14 06:57:48 +0000

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   uploads/f759ab686733ca34c008b93a447f5cde

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require("multer")
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+var upload = multer({ dest: 'uploads/' });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+// Handle the file upload on POST
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
